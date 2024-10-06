b63c02e14907bfe930418e9dde601cd949a3f10f
Amoh-Ntim <amohntim123@gmail.com>
2024-10-06 19:42:07 +0000

    everything done. left with font sora and bottombar

--- Zego.js
+++ Zego.js
@@ -7,7 +7,21 @@ function Zego() {
   return (
     <View style={tw`flex-1`}>
      <View style={tw`bg-[#131313] flex-1 justify-center`}>
-
+     {/* location and image view */}
+       <View style={tw`flex-row justify-between items-center mx-2 pb-8`}>
+       {/* location and name */}
+       <View style={tw``}>
+        <Text style={tw`text-white`}>Location</Text>
+        <Text style={tw`text-white`}>Bilzen, Tanjungbalai</Text>
+       </View>
+       {/* image view */}
+        <View>
+        <Image
+        source={require('./assets/girl.png')}
+        style={tw`w-8 h-8 mr-2`}
+      />
+        </View>
+       </View>
        <Searchbar/>
      </View>
      <View style={tw`bg-white flex-2`}>
